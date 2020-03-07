69a88203d26bb4fa7e5c709a117252a16d0343d1
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:45:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -35,7 +35,7 @@ def take_a_number(katz_deli,person_name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli <= 0
+  if katz_deli.length <= 0
     puts "There is nobody waiting to be served!"
   else 
     current_person = katz_deli[0]
