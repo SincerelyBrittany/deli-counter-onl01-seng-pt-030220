cc4ef3f38ff6cb0c5526c3104f429c6fa699c008
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:10:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@
 # ruby deli_counter.rb
 
 def line(array)
-  current_line = "The line is currently:"
   if array.length <= 0
     puts "The line is currently empty."
   else 
+    current_line = "The line is currently:"
      array.each_with_index do |item, index|
      index +=1
      current_line << " #{index}. #{item}"
