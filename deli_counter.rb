38ad4d84f568c9e83edfa462e27258048552678e
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:08:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 
 def line(array)
   current_line = "The line is currently: "
-  if array.length < = 0
+  if array.length <= 0
     puts "The line is currently empty."
   else array.length > 0
      array.each_with_index do |item, index|
