b5f5a575fb3604d1415313278b4471d6b1845d85
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:33:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,11 +24,11 @@ def take_a_number(katz_deli,person_name)
       welcome_message = "Welcome, #{name}. You are number #{index} in line."
       puts welcome_message
     else 
-    katz_deli.push(person_name)
-    welcome_message = "Welcome, #{person_name}. You are number"
-    katz_deli.each_with_index do |item, index|
-    index +=1
-    welcome_message << " #{index} in line."
+    # katz_deli.push(person_name)
+    # welcome_message = "Welcome, #{person_name}. You are number"
+    # katz_deli.each_with_index do |item, index|
+    # index +=1
+    # welcome_message << " #{index} in line."
    end
    puts welcome_message
 end
