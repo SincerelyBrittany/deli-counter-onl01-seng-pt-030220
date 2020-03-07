cb7adf415494e4d4cdd6e223d7261100d99238eb
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 20:26:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,9 @@ katz_deli = []
 
 def line(katz_deli)
  arr_length = katz_deli.length
- puts "The line is currently:"
+
  if arr_length > 0 
+    puts "The line is currently:"
    katz_deli.each_with_index do |person, index|
   puts  "#{index}. #{person}"
  end
