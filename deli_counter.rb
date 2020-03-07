01ade4cd1ed7c1041868f3dd4fac48076c526962
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:15:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ puts line(["a","b","c"])
 
 def take_a_number(katz_deli,person_name)
     welcome_message = "Welcome, #{person_name}. You are number"
-     array.each_with_index do |item, index|
+     katz_deli.each_with_index do |item, index|
      index +=1
      welcome_message << " #{index} in line.""
    end
