84c94741b856b9755f0e6bd78537723d4a54c712
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 21:27:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 
 def take_a_number(katz_deli,person_name)
-    if person_name != 
+    if person_name included?
     katz_deli.push(person_name)
     welcome_message = "Welcome, #{person_name}. You are number"
     katz_deli.each_with_index do |item, index|
