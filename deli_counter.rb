0034ef19983137f32b545eca1f62281ae5ffe21d
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 20:10:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+# ruby deli_counter.rb
+katz_deli = []
+
+def line(array)
+  if array.length > 0
+  current_line =[]
+  array.each_with_index do |item, index|
+  index +=1
+  current_line << index << item
+  end
+else 
+  puts "The line is currently empty."
+end
+  puts "The line is currently:"
+  puts current_line
+end
+
+line(["a","b","c"])
+
+def take_a_number(line,endofline)
+  
+end
