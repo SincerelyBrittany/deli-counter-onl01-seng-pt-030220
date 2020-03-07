3fae214269ca6b4611ca05ad396ac999ebb133f0
Brittany Russell <sincerelybr@gmail.com>
2020-03-07 20:41:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,11 @@
 def line(array)
   current_line =[]
   if array.length > 0
-  array.each_with_index do |item, index|
-  index +=1
-  index.join(".")
-  current_line << index << ". " << item
+  array.map.with_index { |value, index| value + index }
+  # array.each_with_index do |item, index|
+  # index +=1
+  # index.join(".")
+  # current_line << index << ". " << item
   end
 else 
   puts "The line is currently empty."
